e58d6b5fb2db092363af73ae7876285c7d9c6d10
KIM-GANG-MIN <hamacu6@gmail.com>
2022-03-31 09:57:45 +0000

    it's me

---
+++ csv_copy_machine.py
@@ -0,0 +1,140 @@
+from scapy.all import *
+from collections import Counter
+import pandas as pd
+import csv
+
+
+protocols={1:'icmp', 6:'tcp', 17:'udp'}
+test_PATH = r'./testData.csv'
+train_PATH = r'./trainData.csv'
+new_train_PATH = r'./newTrainData.csv'
+new_test_PATH = r'./newTestData.csv'
+traffic = Counter()
+src_traffic = Counter()
+dst_traffic = Counter()
+
+def size_check(num):
+    try:
+        for x in ['','K', 'M', 'G', 'T']:
+            if num < 1024.: return "%3.1f %sB" % (num, x)
+            num /=1024.
+            return "3.1f PB" % (num)
+    except:
+        return "0 B";
+
+def traffic_monitor_callback(pkt):
+    if IP in pkt:
+        src_ip = pkt[IP].src
+        dst_ip = pkt[IP].dst
+        proto = pkt[IP].proto
+        length = pkt[IP].len
+        src_mac = pkt[0][0].src
+        dst_mac = pkt[0][0].dst
+        attack = str(0) # delete in later
+        category = 'Normal'# delete in later
+        subcategory = 'Normal' #delete in later
+        sport = str(0)
+        dport = str(0)
+        flag = str(0)
+        seq = str(0)
+
+        if proto in protocols:
+            if proto == 1:
+                message_type = pkt[ICMP].type
+                code = pkt[ICMP].code
+                #proto = str(proto)
+                proto = 'ICMP'
+            
+            if proto == 6:
+                sport = str(pkt[TCP].sport)
+                dport = str(pkt[TCP].dport)
+                #proto = str(proto)
+                proto = 'TCP'
+                seq = pkt[TCP].seq
+                ack = pkt[TCP].ack
+                flag = pkt[TCP].flags
+            
+            if proto == 17:
+                sport = str(pkt[UDP].sport)
+                dport = str(pkt[UDP].dport)
+                #proto = str(proto)
+                proto = 'UDP'
+        
+        src_traffic.update({tuple(map(str,src_ip))})
+        dst_traffic.update({tuple(dst_ip)})
+        traffic.update({tuple(map(str, (src_ip, dst_ip, proto, src_mac, dst_mac, sport, dport, flag, seq, attack, category, subcategory))): length})
+
+def add_n_in(df, scan_time):
+    for(h1, h2, proto, src_mac, dst_mac, sport, dport, flag, seq, attack, category, subcategory), total in traffic.most_common():
+        if src_mac == 'ff:ff:ff:ff:ff:ff' : 
+            continue
+        elif dst_mac == 'ff:ff:ff:ff:ff:ff':
+            continue
+
+        for (src_h), num_s  in src_traffic.most_common():
+            if ''.join(src_h) == h1:
+                temp_s = num_s
+                break
+        for (dst_h), num_s in dst_traffic.most_common():
+            if ''.join(dst_h) == h2:
+                temp_d = num_s
+                break
+        data = pd.DataFrame({'saddr': [h1], 'daddr' : [h2], 'proto' : [proto], 'src_mac' : [src_mac], 'dst_mac' : [dst_mac], 'sport' : [sport], 'dport':[dport], 'flag':[flag], 'N_IN_Conn_P_SrcIP':[temp_s], 'N_IN_Conn_P_DstIP':[temp_d], 'length' : [total], 'rate':[float(total)/scan_time], 'seq' : [seq], 'attack' : [attack], 'category' : [category], 'subcategory' : [subcategory]})
+        df = pd.concat([df,data], ignore_index=True)
+    return df
+
+def load_csv(PATH):
+    df_csv = pd.read_csv(PATH)
+    return df_csv
+
+def make_data(original_df, df):
+    df['pkSeqID'] = 0
+    df['stddev'] = 0
+    df['min'] = 0
+    df['max']=0
+    df['mean']=0
+    df['state_num'] = 0 # add in latter
+    df['stddev'] = 0
+    df['drate'] = 0
+    df['srate'] = 0 # add in later
+    df = df.drop(['flag', 'length', 'rate', 'src_mac', 'dst_mac'],axis=1)
+    original_df = pd.concat([original_df, df])
+    return original_df
+
+def init_traffic():
+    global traffic
+    global src_traffic
+    global dst_traffic
+    traffic = Counter()
+    src_traffic = Counter()
+    dst_traffic = Counter()
+
+def do_sniff(ori_df, scan_time):
+    hosts={}
+    df = pd.DataFrame(columns=['saddr', 'daddr', 'proto', 'src_mac', 'dst_mac', 'sport', 'dport', 'flag', 'N_IN_Conn_P_SrcIP', 'N_IN_Conn_P_DstIP', 'rate', 'length', 'seq','attack','category','subcategory'])
+    sniff(prn=traffic_monitor_callback, timeout=scan_time, store=False)
+    df = add_n_in(df, scan_time)
+    ori_df = make_data(ori_df, df)
+    return ori_df
+
+def write_csv(df, PATH):
+    df.to_csv(PATH, index=False)
+
+if __name__ == "__main__":
+    train_df = load_csv(train_PATH)
+    test_df= load_csv(test_PATH)
+    init_traffic()
+    sc_time_one = 300
+    sc_time_two = 100
+    sniff(prn=traffic_monitor_callback,timeout=sc_time_one, store=False)
+    train_df = do_sniff(train_df, sc_time_one)
+    write_csv(train_df, new_train_PATH)
+    init_traffic()
+    sniff(prn=traffic_monitor_callback, timeout= sc_time_two, store=False)
+    test_df = do_sniff(test_df, sc_time_two)
+    write_csv(test_df, new_test_PATH)
+    
+
+
+
+
--- packet_check.py
+++ packet_check.py
@@ -1,63 +1,117 @@
 from scapy.all import *
 from collections import Counter
 import pandas as pd
+import csv
 
-sample_interval = 60
+
+sample_interval = 10
 protocols={1:'icmp', 6:'tcp', 17:'udp'}
+test_PATH = r'./testData.csv'
+train_PATH = r'./trainData.csv'
 
 traffic = Counter()
+src_traffic = Counter()
 hosts={}
-data = []
-df = pd.DataFrame(data, columns=['src_ip', 'dst_ip', 'proto', 'length', 'src_mac', 'dst_mac', 'sport', 'dport', 'seq', 'flag'])
+dst_traffic = Counter()
+#data = {'src_ip': ['init'], 'dst_ip' : ['init'], 'proto' : ['init'], 'src_mac' : ['init'], 'dst_mac' : ['init'], 'sport' : ['init'], 'dport':['init'], 'flag':['init'], 'ack':['init'], 'n_in_src':[0], 'n_in_dst':[0], 'rate':[0]}
+df = pd.DataFrame(columns=['saddr', 'daddr', 'proto', 'src_mac', 'dst_mac', 'sport', 'dport', 'flag', 'n_in_src', 'n_in_dst', 'rate', 'length', 'seq','attack','category','subcategory'])
 
 def size_check(num):
-    for x in ['','K', 'M', 'G', 'T']:
-        if num < 1024.: return "%3.1f %sB" % (num, x)
-        num /=1024.
-        return "3.1f PB" % (num)
+    try:
+        for x in ['','K', 'M', 'G', 'T']:
+            if num < 1024.: return "%3.1f %sB" % (num, x)
+            num /=1024.
+            return "3.1f PB" % (num)
+    except:
+        return "0 B";
 
 def traffic_monitor_callback(pkt):
     if IP in pkt:
         src_ip = pkt[IP].src
         dst_ip = pkt[IP].dst
-        porto = pkt[IP].proto
+        proto = pkt[IP].proto
         length = pkt[IP].len
-        src_mac = packet[0][0].src
-        dst_mac = packet[0][0].dst
-    
+        src_mac = pkt[0][0].src
+        dst_mac = pkt[0][0].dst
+        attack = str(0) # delete in later
+        category = 'Normal'# delete in later
+        subcategory = 'Normal' #delete in later
+        sport = str(0)
+        dport = str(0)
+        flag = str(0)
+        seq = str(0)
+
         if proto in protocols:
             if proto == 1:
                 message_type = pkt[ICMP].type
                 code = pkt[ICMP].code
-                traffic.update({tuple(sorted(map(atol, (pkt.src, pkt.dst)))): pkt.len})
-                data_to_insert = {}
+                #proto = str(proto)
+                proto = 'ICMP'
+            
             if proto == 6:
-                sport = pkt[TCP].sport
-                dport = pkt[TCP].dport
+                sport = str(pkt[TCP].sport)
+                dport = str(pkt[TCP].dport)
+                #proto = str(proto)
+                proto = 'TCP'
                 seq = pkt[TCP].seq
                 ack = pkt[TCP].ack
                 flag = pkt[TCP].flags
-
+            
             if proto == 17:
-                sport = pkt[UDP].sport
-                dport = pkt[UDP].dport
+                sport = str(pkt[UDP].sport)
+                dport = str(pkt[UDP].dport)
+                seq = pkt[UDP].seq
+                #proto = str(proto)
+                proto = 'UDP'
+        
+        src_traffic.update({tuple(map(str,src_ip))})
+        dst_traffic.update({tuple(dst_ip)})
+        traffic.update({tuple(map(str, (src_ip, dst_ip, proto, src_mac, dst_mac, sport, dport, flag, seq, attack, category, subcategory))): length})
 
-        traffic.update({tuple(sorted(map(atol, (pkt.src, pkt.dst)))): pkt.len})
+def add_n_in():
+    global df
+    for(h1, h2, proto, src_mac, dst_mac, sport, dport, flag, seq, attack, category, subcategory), total in traffic.most_common():
+        if src_mac == 'ff:ff:ff:ff:ff:ff' : 
+            continue
+        elif dst_mac == 'ff:ff:ff:ff:ff:ff':
+            continue
 
-sniff(prn=traffic_monitor_callback,timeout=sample_interval, store=False)
+        for (src_h), num_s  in src_traffic.most_common():
+            if ''.join(src_h) == h1:
+                temp_s = num_s
+                break
+        for (dst_h), num_s in dst_traffic.most_common():
+            if ''.join(dst_h) == h2:
+                temp_d = num_s
+                break
+        data = pd.DataFrame({'saddr': [h1], 'daddr' : [h2], 'proto' : [proto], 'src_mac' : [src_mac], 'dst_mac' : [dst_mac], 'sport' : [sport], 'dport':[dport], 'flag':[flag], 'n_in_src':[temp_s], 'n_in_dst':[temp_d], 'length' : [total], 'rate':[float(total)/sample_interval], 'seq' : [seq], 'attack' : [attack], 'category' : [category], 'subcategory' : [subcategory]})
+        df = pd.concat([df,data], ignore_index=True)
 
-for(h1,h2), total in traffic.most_common():
-    h1, h2 = map(ltoa, (h1,h2))
-    for host in (h1,h2):
-        if host not in hosts:
-            try:
-                rhost = socket. gethostbyaddr(host)
-                hosts[host] = rhost[0]
-            except:
-                hosts[host] = None
+def load_csv(PATH):
+    df_csv = pd.read_csv(PATH)
+    return df_csv
 
-    h1 = "%s (%s)" % (hosts[h1], h1) if hosts[h1] is not  None else h1
-    h2 = "%s (%s)" % (hosts[h2], h2) if hosts[h2] is not None else h2
-    print("%s/s: %s - %s" % (size_check(float(total)/sample_interval), h1, h2))
-    print("Try num: %s" %(total))
-    
+def make_data(original_df):
+    global df
+    df['pkSeqID'] = 0
+    df['stddev'] = 0
+    df['min'] = 0
+    df['max']=0
+    df['mean']=0
+    df['state_num'] = 0 # add in latter
+    df['stddev'] = 0
+    df['drate'] = 0
+    df['srate'] = 0 # add in later
+    df = df.drop(['flag', 'length', 'rate', 'src_mac', 'dst_mac'])
+    original_df = pd.concat([orginal_df, df])
+
+if __name__ == "__main__":
+    train_df = load_csv(trainPATH)
+    test_df= load_csv(testPATH)
+
+    sample_interval = 10
+    sniff(prn=traffic_monitor_callback,timeout=sample_interval, store=False)
+    add_n_in()
+
+
+
