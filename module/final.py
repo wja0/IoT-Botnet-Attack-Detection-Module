e29c659518064bdc0bf3b8a1d2652e9a32e5ef1c
Kim-Gang-Min <63631095+Kim-Gang-Min@users.noreply.github.com>
2022-05-26 16:54:33 +0900

    c
    c

--- module/final.py
+++ module/final.py
@@ -1,4 +1,6 @@
 import ids_ap as ids
+import model_check as mdc
+improt client as srv
 import os
 from requests import get
 
@@ -6,24 +8,23 @@ from requests import get
 if __name__ == "__main__":
     host_ip = get("https://api.ipify.org").text
     attack = False
-    c_socket = connection_server()
-    model_init()
+    c_socket = srv.connection_server()
+    scan_model, atk_model = mdc.model_init()
     
     while(True):
-        pd_csv = scan_data()
+        traffic_df = ids.scan_data()
         
         if os.fork() == 0:
             err = check_server(c_socket)
             
             if err:
                 print("Can't connect to secure server!")
-                
-            write_csv(pd_csv)
-            attack = check_attack()
+
+            attack, bad_traffic = mdc.check_attack()
             
             if attack:
                 infrom_usr()
-                send_to_server()
+                srv.send_to_server()
             
             return #child process down
         
