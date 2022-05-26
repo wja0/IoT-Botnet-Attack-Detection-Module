8103fe9e32147b233f03b322d092ad361e7f7559
Kim-Gang-Min <63631095+Kim-Gang-Min@users.noreply.github.com>
2022-05-26 16:49:55 +0900

    c
    c

--- module/ids_ap.py
+++ module/ids_ap.py
@@ -26,17 +26,16 @@ dst_traffic = Counter()
 def traffic_monitor_callback(pkt):
     if IP in pkt:
         proto = pkt[IP].proto #protocol
-        attack = '?'
         
-        if proto not in protocols: # This is not attack of Mirai
-            return
         
         src_ip = pkt[IP].src #saddr
         dst_ip = pkt[IP].dst #daddr
         host_ip = get("https://api.ipify.org").text
         
-        if src_ip != host_ip and dst_ip != host_ip:
-            return 
+        if str(src_ip) != host_ip:
+            if str(dst_ip) != host_ip:
+                if str(src_ip) not in '192.168.0.':
+                    return 
         
         time = pkt[IP].time # time_atr
         length = pkt[IP].len #length
@@ -46,21 +45,19 @@ def traffic_monitor_callback(pkt):
         dport = str(0)
         flag = 0
         flags = 0
-            
-        try:
-            a = pkt[DNS]
-            # get a domain addr
-            dns = 1
-        except:
-            dns = 0
-            
-        if proto == 6:
+        dns = 0
+        
+        if proto ==1:
+            proto = 5 #icmp
+        elif proto == 2:
+            proto = 4 #igmp 
+        elif proto == 6:
             sport = str(pkt[TCP].sport)
             dport = str(pkt[TCP].dport)
-            proto = 'tcp'
+            proto = 0 #tcp
             flag = pkt[TCP].flags
             
-        if proto == 17:
+        elif proto == 17:
             try:
                 sport = str(pkt[UDP].sport)
             except:
@@ -70,19 +67,21 @@ def traffic_monitor_callback(pkt):
                 dport = str(pkt[UDP].dport)
             except:
                 sport = str(0)
-            proto = 'udp'
+            proto = 1 #udp
                 
-        if proto == 47:
+        elif proto == 47:
             flag = pkt[GRE].flags
-            proto = 'gre'
-
+            proto = 2 #gre
+        elif proto == 89:
+            proto = 3 # ospfigp
+            
         src_traffic.update({tuple(src_ip)}) # get conn_p_src_ip
         dst_traffic.update({tuple(dst_ip)}) # get conn_p_dst_ip
         check_time(src_ip, dst_ip, dport, time) # get time and datarate
         flags =  flag_to_int(flag)
         
-        traffic.update({tuple(map(str, (src_ip, dst_ip, proto, src_mac, dst_mac, sport, dport, flags, dns, attack))): length})
-        print(src_ip," ", dst_ip, " ",proto, " ",src_mac, " ",dst_mac, " ",sport, " ",dport, " ",flags, " ",dns," ", attack)
+        traffic.update({tuple(map(str, (src_ip, dst_ip, proto, src_mac, dst_mac, sport, dport, flags, dns))): length})
+        print(src_ip," ", dst_ip, " ",proto, " ",src_mac, " ",dst_mac, " ",sport, " ",dport, " ",flags, " ",dns)
 
 def flag_to_int(flag):
     flags = 0
@@ -102,7 +101,7 @@ def flag_to_int(flag):
 
 def add_n_in(df, scan_time):
     time = 1
-    for(h1, h2, proto, src_mac, dst_mac, sport, dport, flag,dns, attack), total in traffic.most_common(): # except broadcast
+    for(h1, h2, proto, src_mac, dst_mac, sport, dport, flag,dns), total in traffic.most_common(): # except broadcast
         if src_mac == 'ff:ff:ff:ff:ff:ff' : 
             continue
         elif dst_mac == 'ff:ff:ff:ff:ff:ff':
@@ -126,7 +125,7 @@ def add_n_in(df, scan_time):
                     time = i.l_time - i.s_time
                 break
 
-        data = pd.DataFrame({'saddr': [h1], 'daddr' : [h2], 'proto' : [proto], 'src_mac' : [src_mac], 'dst_mac' : [dst_mac], 'sport' : [sport], 'dport':[dport], 'flag':[flag], 'N_IN_Conn_P_SrcIP':[temp_s], 'N_IN_Conn_P_DstIP':[temp_d], 'length' :[total] , 'time' : [time], 'datarate' : [total/time], 'dns':[dns], 'category' : [attack]})
+        data = pd.DataFrame({'saddr': [h1], 'daddr' : [h2], 'proto' : [proto], 'src_mac' : [src_mac], 'dst_mac' : [dst_mac], 'sport' : [sport], 'dport':[dport], 'flag':[flag], 'N_IN_Conn_P_SrcIP':[temp_s], 'N_IN_Conn_P_DstIP':[temp_d], 'length' :[total] , 'time' : [time], 'datarate' : [total/time], 'dns':[dns]})
         df = pd.concat([df,data], ignore_index=True)
     return df
 
@@ -141,7 +140,7 @@ def init_traffic():
     time_check.append(tmp)
 
 def do_sniff(scan_time):
-    df = pd.DataFrame(columns=['saddr', 'daddr', 'proto', 'src_mac', 'dst_mac', 'sport', 'dport', 'flag', 'N_IN_Conn_P_SrcIP', 'N_IN_Conn_P_DstIP', 'length', 'time', 'datarate','dns', 'category'])
+    df = pd.DataFrame(columns=['saddr', 'daddr', 'proto', 'src_mac', 'dst_mac', 'sport', 'dport', 'flag', 'N_IN_Conn_P_SrcIP', 'N_IN_Conn_P_DstIP', 'length', 'time', 'datarate','dns'])
     sniff(iface="br0",prn=traffic_monitor_callback, timeout = scan_time, store=False)
     df = add_n_in(df, scan_time)
     
@@ -167,13 +166,7 @@ def check_time(src_ip, dst_ip, dport, n_time):
 
 def scan_data():
     init_traffic()
-    sc_time_one = 600
-    train_df = do_sniff(sc_time_one)
-    write_csv(train_df, './nomal_traffic_cate.csv')
+    sc_time_one = 60
+    df = do_sniff(sc_time_one)
+    return df
                 
-
-if __name__ == "__main__":
-    init_traffic()
-    sc_time_one = 600
-    train_df = do_sniff(sc_time_one)
-    write_csv(train_df, './nomal_traffic_cate.csv')
